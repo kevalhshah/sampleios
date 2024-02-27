64915abe3303247bf4567856ef7d36f73dceb53e
Keval Shah <kevalmotog@gmail.com>
2024-02-27 09:21:42 -0500

    initial

---
+++ SampleiOS/SampleiOS/SampleiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleiOSApp.swift
+//  SampleiOS
+//
+//  Created by Keval Shah on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
